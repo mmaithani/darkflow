828e2d25a760c05aeb0b971788e3a3d4141f3c3b
mmaithani <mohitmaithani81@gmail.com>
2020-02-23 12:24:25 +0530

    added new trial files

--- imaageai.py
+++ imaageai.py
@@ -1,17 +1,16 @@
-# # imaageai.py
+# imaageai.py
 
-# from imageai.Detection import VideoObjectDetection
-# import os
+from imageai.Detection import VideoObjectDetection
+import os
 
-# execution_path = os.getcwd()
+execution_path = os.getcwd()
 
-# detector = VideoObjectDetection()
-# detector.setModelTypeAsYOLOv3()
-# detector.setModelPath( os.path.join(execution_path , "yolo.h5"))
-# detector.loadModel()
+detector = VideoObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath( os.path.join(execution_path , "yolo.h5"))
+detector.loadModel()
 
-# video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join(execution_path, "traffic.mp4"),
-#                             output_file_path=os.path.join(execution_path, "traffic_detected")
-#                             , frames_per_second=20, log_progress=True)
-# print(video_path)
-!pip install tensorflow
+video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join(execution_path, "traffic.mp4"),
+                            output_file_path=os.path.join(execution_path, "traffic_detected")
+                            , frames_per_second=20, log_progress=True)
+print(video_path)
